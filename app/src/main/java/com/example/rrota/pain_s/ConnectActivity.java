2af1f4d7a2e4f7179af28f677d6bbc75cae9bc31
emil4443 <conq122@mail.ru>
2018-04-12 13:10:45 +0300

    12.04.2018 13:10
    активити для связи с нами(настрою на сервере и будет работать)
    и по мелочам

---
+++ app/src/main/java/com/example/rrota/pain_s/ConnectActivity.java
@@ -0,0 +1,37 @@
+package com.example.rrota.pain_s;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ConnectActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+        webView = (WebView) findViewById(R.id.webWiew2);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://ecoselect.ru/connect/");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
